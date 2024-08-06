671b99d91d050705d3befda81c4318c1b4ab0fcc
ChoiHaeHyuk <88603321+hyuk12@users.noreply.github.com>
2024-08-06 15:21:05 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "hyuko",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/cat.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop everything using java.",
     email: "gogur7419@gmail.com",
