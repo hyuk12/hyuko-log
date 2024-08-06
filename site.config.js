c3c9c892300f2e862a422fee5459f2f670f2b43e
ChoiHaeHyuk <88603321+hyuk12@users.noreply.github.com>
2024-08-06 15:21:44 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ const CONFIG = {
   // blog setting (required)
   blog: {
     title: "hyuko-log",
-    description: "welcome to morethan-log!",
+    description: "welcome to hyuko-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
